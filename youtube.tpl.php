c33111546b60c8c8e16a3adef511f51eb2fbdadc
nosov33 <ivan@inosov.ru>
2015-04-15 11:46:34 +0300

    Themisation of .tpl file

--- youtube.tpl.php
+++ youtube.tpl.php
@@ -1,3 +1,12 @@
-<h2><?php print $title1; ?></h2>
-<h2><?php print $title2; ?></h2>
-<h2><?php print $videoURL; ?></h2>
+<section class="content-section video-section">
+  <div class="pattern-overlay">
+  <a id="bgndVideo" class="player" data-property="{videoURL:'<?php print $videoURL; ?>',containment:'.video-section', quality:'highres', autoPlay:true, mute:true, opacity:1}">bg</a>
+    <div class="container">
+		<div class="col-lg-12">
+			<h1><?php print $title1; ?></h1>
+			<center><?php print $title2; ?></center>
+		</div>
+
+    </div>
+  </div>
+</section>
