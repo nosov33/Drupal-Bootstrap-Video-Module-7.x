52b7ffdc52ea7e24407bb8548710dd6ce89a6c8c
nosov33 <ivan@inosov.ru>
2015-04-15 12:00:32 +0300

    Summary
    - added 'https'
    - add css way to info file
    - edited css to height 500px
    - add init script to the .tpl

--- youtube.tpl.php
+++ youtube.tpl.php
@@ -1,12 +1,17 @@
+
 <section class="content-section video-section">
   <div class="pattern-overlay">
-  <a id="bgndVideo" class="player" data-property="{videoURL:'<?php print $videoURL; ?>',containment:'.video-section', quality:'highres', autoPlay:true, mute:true, opacity:1}">bg</a>
+  <a id="bgndVideo" class="player" data-property="{videoURL:'https://<?php print $videoURL; ?>',containment:'.video-section', quality:'highres', autoPlay:true, mute:true, opacity:1}">bg</a>
     <div class="container">
 		<div class="col-lg-12">
 			<h1><?php print $title1; ?></h1>
 			<center><?php print $title2; ?></center>
 		</div>
-
     </div>
   </div>
 </section>
+<script type="text/javascript">
+jQuery( document ).ready(function( $ ) {
+    $(".player").mb_YTPlayer();
+});
+</script>
